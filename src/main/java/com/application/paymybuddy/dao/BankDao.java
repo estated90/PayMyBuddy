c86badb5208e9d66bb57502e96b5d3056c9c2e93
estated90 <nicolas.de-testa@hotmail.fr>
2020-12-03 20:07:44 +0100

    add repository tag to two dao

--- src/main/java/com/application/paymybuddy/dao/BankDao.java
+++ src/main/java/com/application/paymybuddy/dao/BankDao.java
@@ -3,6 +3,7 @@ package com.application.paymybuddy.dao;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.application.paymybuddy.model.Bank;
 
@@ -10,6 +11,7 @@ import com.application.paymybuddy.model.Bank;
  * @author nicolas
  *
  */
+@Repository
 public interface BankDao extends JpaRepository<Bank, UUID>{
 
 }
--- src/main/java/com/application/paymybuddy/dao/ConnectionsDao.java
+++ src/main/java/com/application/paymybuddy/dao/ConnectionsDao.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.application.paymybuddy.model.Connections;
 import com.application.paymybuddy.model.Holder;
@@ -13,6 +14,7 @@ import com.application.paymybuddy.model.Holder;
  * @author nicolas
  *
  */
+@Repository
 public interface ConnectionsDao extends JpaRepository<Connections, UUID> {
 
 	@Query("SELECT c FROM Connections c WHERE holderId = :mainHolder AND friendId=:friend")
